0f93da3aa52edca95ec99d9116f87ad4cc456191
ali <alimostafaam2004@gmail.com>
2025-12-14 22:29:10 +0200

    feat(rating): add rating DTOs

---
+++ DTOs/Rating/CreateRatingDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pikmi.API.DTOs.Rating
+{
+    public class CreateRatingDto
+    {
+        [Required]
+        public int RideId { get; set; }
+
+        [Required]
+        public string RatedUserId { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int RatingValue { get; set; }
+
+        [MaxLength(1000)]
+        public string? Comment { get; set; }
+    }
+}
---
+++ DTOs/Rating/RatingResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Pikmi.API.DTOs.Rating
+{
+    public class RatingResponseDto
+    {
+        public int RatingId { get; set; }
+        public int RideId { get; set; }
+        public string RaterId { get; set; }
+        public string RaterName { get; set; }
+        public string RaterProfileImage { get; set; }
+        public string RatedUserId { get; set; }
+        public string RatedUserName { get; set; }
+        public int RatingValue { get; set; }
+        public string Comment { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
